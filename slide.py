9bd36cdbfc1756892085199848567b60b8236f22
vinay sawant <sawantvinayr@gmail.com>
2012-09-23 13:12:25 +0530

    sys

--- slide.py
+++ slide.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import sys
+
 def openfile(fname):								#open file in read mode
     f=open(fname,"r")								#
     window_size=int(f.readline())					#read first line i.e (sliding window size)
@@ -10,10 +12,9 @@ fname=raw_input("Enter filename: => ")				#input from user (filename)
 window_size,stream=openfile(fname)					#called function 'openfile' to read file
 
 stream_a=[int(i) for i in stream.split()]			#converted input stream into array
-output=[]
 
 while len(stream_a)-window_size+1: 					#
-	output.append(min(stream_a[:window_size]))		#find minimum element
+	l=min(stream_a[:window_size])					#find minimum element
+	sys.stdout.write("%d " %l)
+	sys.stdout.flush()
 	stream_a.pop(0)									#poped first element and again find min(window_size)
-		
-print output										#print output
